b50695d2c04099f216ca1f117b3a16c91569c65d
Nick Bourdakos <bourdakos1@gmail.com>
2015-08-22 18:55:47 -0400

    l

--- app/src/main/java/com/xlythe/spotifysteamer/MainActivity.java
+++ app/src/main/java/com/xlythe/spotifysteamer/MainActivity.java
@@ -14,6 +14,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
         if (savedInstanceState == null) {
             getSupportFragmentManager().beginTransaction()
                     .add(R.id.fragment_search, new SearchFragment())
